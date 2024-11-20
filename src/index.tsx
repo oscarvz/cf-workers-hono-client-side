fe207b6a943e824b75ff6328f838c8e3d45a7b07
oscarvz <oscarvz@users.noreply.github.com>
2024-11-20 16:41:51 +0100

    General cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,22 @@
 import { Hono } from "hono";
-import type { JSX } from "hono/jsx";
+import { Style } from "hono/css";
 import { jsxRenderer } from "hono/jsx-renderer";
-import type { Manifest } from "vite";
 
 import { Counter } from "./client/Counter";
-import { Style } from "hono/css";
+import { getAssetImportTagsFromManifest } from "./utils";
 
 const app = new Hono();
 
-app.get(
+app.use(
 	"/",
 	jsxRenderer(
 		({ children }) => {
-			const assetImportTags = (() => {
-				if (!import.meta.env.PROD) {
-					return <script type="module" src="/src/client/index.tsx" />;
-				}
-
-				const rootManifest = import.meta.glob<{ default: Manifest }>(
-					"/public/.vite/manifest.json",
-					{ eager: true },
-				);
-
-				const manifest = Object.values(rootManifest).at(0)?.default;
-				if (!manifest) {
-					return null;
-				}
-
-				const importTags: Array<
-					JSX.IntrinsicElements["link"] | JSX.IntrinsicElements["script"]
-				> = [];
-
-				for (const { file, css } of Object.values(manifest)) {
-					const scriptTag = <script key={file} type="module" src={file} />;
-					importTags.push(scriptTag);
-
-					if (css && css.length > 0) {
-						const cssTags = css.map((cssPath) => (
-							<link key={cssPath} rel="stylesheet" href={cssPath} />
-						));
-						importTags.push(cssTags);
-					}
-				}
-
-				return importTags;
-			})();
+			const assetImportTags = getAssetImportTagsFromManifest();
 
 			return (
 				<html lang="en">
 					<head>
-						<title>Wow bindings</title>
+						<title>Wow cf-workers-hono-client-side</title>
 						<link rel="icon" href="/favicon.svg" />
 						<Style />
 						{assetImportTags}
@@ -63,7 +30,8 @@ app.get(
 		},
 		{ docType: true },
 	),
-	(c) => c.render(<Counter />),
 );
+
+app.get("/", (c) => c.render(<Counter />));
 
 export default app;
